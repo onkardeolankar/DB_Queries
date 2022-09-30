0f6ba403982a407d723152de0e0e466b3a8cb265
onkardeolankar1996@gmail.com <codewithkeeda@gmail.com>
2022-09-30 12:32:03 +0530

    initial changes

---
+++ Queries - 32/index.js
@@ -0,0 +1,226 @@
+db.zenclassdata.insertMany([
+  {
+    users:
+      "Dhanesh Surve , santosh akkar , rahul patil , saurabh joshi , vedant patel , riya gupta , shubham shah , omkar khanvilkar , priya rout , akshay rathod , vishal salve , priyanka patel , komal mapari , shraddha sharma , aditya kapoor , charul yeole , gaurav ratnakar , richa joshi , ashok jadhav , kapil dev",
+    codekata:
+      "55 , 23 , 63 , 36 , 55 , 35 , 95 , 80 , 15 , 21 , 51 , 65 , 70 , 85 , 75 , 57 , 66 , 70 , 110 , 40",
+    attendance: { present: 20, absent: 0, date: 6 },
+    topics: "Basic Javascript Concepts",
+    tasks: {
+      name: "Write a code to rotate an array by 3 times",
+      submit: 12,
+      notsubmit: 7,
+    },
+    company_drives: {
+      name: "Accenture",
+      date: 11,
+      user_appeared:
+        "Dhanesh Surve , santosh akkar , rahul patil , saurabh joshi , vedant patel , riya gupta , shubham shah , omkar khanvilkar , priya rout , akshay rathod , vishal salve , priyanka patel , komal mapari , shraddha sharma , aditya kapoor , charul yeole , gaurav ratnakar",
+    },
+    mentors: { name: "ajay mishra", mentee: 22 },
+  },
+
+  {
+    users:
+      "Dhanesh Surve , santosh akkar , rahul patil , saurabh joshi , riya gupta , shubham shah , omkar khanvilkar , priya rout , akshay rathod , vishal salve , priyanka patel , komal mapari",
+    codekata: "63 , 10 , 35 , 85 , 20 , 50 , 45 , 40 , 15 , 21 , 51 , 65 ",
+    attendance: { present: 11, absent: 2, date: 7 },
+    topics: "Advance Javascript",
+    tasks: {
+      name: "Write a code to reverse a string input",
+      submit: 8,
+      notsubmit: 2,
+    },
+    company_drives: {
+      name: "Deloite",
+      date: 14,
+      user_appeared:
+        "Dhanesh Surve , santosh akkar , rahul patil , saurabh joshi , vedant patel , riya gupta , shubham shah , omkar khanvilkar , priya rout",
+    },
+    mentors: { name: "viraj deshmukh", mentee: 13 },
+  },
+
+  {
+    users:
+      "Dhanesh Surve , santosh akkar , rahul patil , saurabh joshi , vedant patel , riya gupta , shubham shah , omkar khanvilkar , priya rout , akshay rathod , vishal salve , priyanka patel , komal mapari , shraddha sharma , aditya kapoor , charul yeole",
+    codekata:
+      "65 , 12 , 45 , 55 , 50 , 100 , 45 , 80 , 15 , 21 , 51 , 65 , 70 , 85 , 75 , 57",
+    attendance: { present: 12, absent: 4, date: 8 },
+    topics: "HTML & CSS",
+    tasks: {
+      name: "Design a navigation bar using HTML & CSS",
+      submit: 10,
+      notsubmit: 2,
+    },
+    company_drives: {
+      name: "Cognizent",
+      date: 15,
+      user_appeared:
+        "Dhanesh Surve , santosh akkar , rahul patil , saurabh joshi , vedant patel , riya gupta , shubham shah , omkar khanvilkar , priya rout , akshay rathod",
+    },
+    mentors: { name: "Gargi Roy", mentee: 16 },
+  },
+
+  {
+    users:
+      "vedant patel , riya gupta , shubham shah , omkar khanvilkar , priya rout , akshay rathod , vishal salve , priyanka patel , komal mapari , shraddha sharma , aditya kapoor , charul yeole , gaurav ratnakar , richa joshi , ashok jadhav , kapil dev",
+    codekata:
+      "50 , 100 , 45 , 80 , 15 , 21 , 51 , 65 , 70 , 85 , 75 , 57 , 66 , 70 , 110 , 40",
+    attendance: { present: 13, absent: 3, date: 15 },
+    topics: "Bootstrap",
+    tasks: {
+      name: "Design a web page using Bootstrap",
+      submit: 10,
+      notsubmit: 3,
+    },
+    company_drives: {
+      name: "Amazon",
+      date: 16,
+      user_appeared:
+        "vedant patel , riya gupta , shubham shah , omkar khanvilkar , priya rout , akshay rathod , vishal salve , priyanka patel , komal mapari , shraddha sharma , aditya kapoor",
+    },
+    mentors: { name: "Gopal Anand", mentee: 16 },
+  },
+
+  {
+    users:
+      "Dhanesh Surve , santosh akkar , rahul patil , saurabh joshi , vedant patel , riya gupta , shubham shah , omkar khanvilkar , priya rout , akshay rathod , vishal salve , priyanka patel , komal mapari , shraddha sharma , aditya kapoor , charul yeole , gaurav ratnakar , richa joshi , ashok jadhav , kapil dev",
+    codekata:
+      "65 , 12 , 45 , 55 , 50 , 100 , 45 , 80 , 15 , 21 , 51 , 65 , 70 , 85 , 75 , 57 , 66 , 70 , 110 , 40",
+    attendance: { present: 16, absent: 4, date: 9 },
+    topics: "ReactJS",
+    tasks: {
+      name: "Write a code to design a shopping App",
+      submit: 14,
+      notsubmit: 2,
+    },
+    company_drives: {
+      name: "Infosys",
+      date: 17,
+      user_appeared:
+        "Dhanesh Surve , santosh akkar , rahul patil , saurabh joshi , vedant patel , riya gupta , shubham shah , omkar khanvilkar , priya rout , akshay rathod , vishal salve , priyanka patel , komal mapari , shraddha sharma",
+    },
+    mentors: { name: "Ramesh Bhardwaj", mentee: 20 },
+  },
+
+  {
+    users:
+      "santosh akkar , rahul patil , saurabh joshi , vedant patel , riya gupta , shubham shah , omkar khanvilkar , priya rout , akshay rathod , vishal salve , priyanka patel , komal mapari , shraddha sharma , aditya kapoor , charul yeole , gaurav ratnakar , richa joshi , ashok jadhav , kapil dev",
+    codekata:
+      "12 , 45 , 55 , 50 , 100 , 45 , 80 , 15 , 21 , 51 , 65 , 70 , 85 , 75 , 57 , 66 , 70 , 110 , 40",
+    attendance: { present: 17, absent: 2, date: 16 },
+    topics: "SQL",
+    tasks: {
+      name: "Write the queries for a school database",
+      submit: 12,
+      notsubmit: 5,
+    },
+    company_drives: {
+      name: "Flipkart",
+      date: 18,
+      user_appeared:
+        "santosh akkar , rahul patil , saurabh joshi , vedant patel , riya gupta , shubham shah , omkar khanvilkar , priya rout , akshay rathod , vishal salve , priyanka patel , komal mapari , shraddha sharma , aditya kapoor , charul yeole , gaurav ratnakar",
+    },
+    mentors: { name: "Raghav Kumar", mentee: 19 },
+  },
+
+  {
+    users:
+      "priya rout , akshay rathod , vishal salve , priyanka patel , komal mapari , shraddha sharma , aditya kapoor , charul yeole , gaurav ratnakar , richa joshi , ashok jadhav , kapil dev",
+    codekata: "15 , 21 , 51 , 65 , 70 , 85 , 75 , 57 , 66 , 70 , 110 , 40",
+    attendance: { present: 10, absent: 2, date: 17 },
+    topics: "MySQL",
+    tasks: {
+      name: "create a IMDB design of any 5 movies",
+      submit: 8,
+      notsubmit: 2,
+    },
+    company_drives: {
+      name: "Citi",
+      date: 19,
+      user_appeared:
+        "priya rout , akshay rathod , vishal salve , priyanka patel , komal mapari , shraddha sharma , gaurav ratnakar , richa joshi",
+    },
+    mentors: { name: "Sam Anand", mentee: 12 },
+  },
+  {
+    users:
+      "vishal salve , priyanka patel , komal mapari , shraddha sharma , aditya kapoor , charul yeole , gaurav ratnakar , richa joshi , ashok jadhav , kapil dev",
+    codekata: "51 , 65 , 70 , 85 , 75 , 57 , 66 , 70 , 110 , 40",
+    attendance: { present: 8, absent: 2, date: 18 },
+    topics: "MangoDB",
+    tasks: {
+      name: "apply CRUD in node js and connect with mongodb atlas",
+      submit: 6,
+      notsubmit: 2,
+    },
+    company_drives: {
+      name: "ZOHO",
+      date: 22,
+      user_appeared:
+        "vishal salve , priyanka patel , komal mapari , charul yeole , gaurav ratnakar , kapil dev",
+    },
+    mentors: { name: "Ganesh Murti", mentee: 10 },
+  },
+
+  {
+    users:
+      "Dhanesh Surve , saurabh joshi , vedant patel , riya gupta , shubham shah , omkar khanvilkar , priya rout , akshay rathod , vishal salve , priyanka patel , komal mapari , shraddha sharma , aditya kapoor , charul yeole , gaurav ratnakar , richa joshi , ashok jadhav , kapil dev",
+    codekata:
+      "65 , 55 , 50 , 100 , 45 , 80 , 15 , 21 , 51 , 65 , 70 , 85 , 75 , 57 , 66 , 70 , 110 , 40",
+    attendance: { present: 16, absent: 2, date: 29 },
+    topics: "Node JS",
+    tasks: {
+      name: "Write a code to excecute the file system task",
+      submit: 16,
+      notsubmit: 0,
+    },
+    company_drives: {
+      name: "Info-Bytes",
+      date: 23,
+      user_appeared:
+        "Dhanesh Surve , saurabh joshi , vedant patel , riya gupta , shubham shah , omkar khanvilkar , priya rout , akshay rathod , vishal salve , priyanka patel , komal mapari , shraddha sharma , gaurav ratnakar , richa joshi , ashok jadhav , kapil dev",
+    },
+    mentors: { name: "Rajendra Prasad", mentee: 18 },
+  },
+
+  {
+    users:
+      "saurabh joshi , vedant patel , riya gupta , shubham shah , omkar khanvilkar , priya rout , akshay rathod , vishal salve , priyanka patel , komal mapari , shraddha sharma , aditya kapoor , charul yeole , gaurav ratnakar , richa joshi , ashok jadhav , kapil dev",
+    codekata:
+      "55 , 50 , 100 , 45 , 80 , 15 , 21 , 51 , 65 , 70 , 85 , 75 , 57 , 66 , 70 , 110 , 40",
+    attendance: { present: 15, absent: 2, date: 30 },
+    topics: "Node-JS and Express-JS",
+    tasks: {
+      name: "Write a code to execute the CRUD operations in NodeJS",
+      submit: 10,
+      notsubmit: 5,
+    },
+    company_drives: {
+      name: "Tech Mahindra",
+      date: 24,
+      user_appeared:
+        "saurabh joshi , vedant patel , riya gupta , shubham shah , omkar khanvilkar , priya rout , akshay rathod , vishal salve , priyanka patel , komal mapari",
+    },
+    mentors: { name: "Shyam Prasad", mentee: 17 },
+  },
+]);
+
+
+//1. Find all the topics and tasks which are thought in the month of October
+db.zenclassdata.find({},{_id:0,topics:1,tasks:{name:1}}).pretty()
+
+//2. Find all the company drives which appeared between 15 oct-2020 and 31-oct-2020
+db.zenclassdata.find({"company_drives.date": {$gte: 15,$lte: 31}},{_id: 0,company_drives: {name: 1,date: 1}}).pretty()
+
+//3. Find all the company drives and students who are appeared for the placement.
+db.zenclassdata.find({},{_id:0,company_drives:{user_appeared:1,name:1}}).pretty()
+
+//4. Find the number of problems solved by the user in codekata
+db.zenclassdata.find({},{_id:0,users:1,codekata:1}).pretty()
+
+//5. Find all the mentors with who has the mentee's count more than 15
+db.zenclassdata.find({"mentors.mentee":{$gt:15}},{_id:0,mentors:1}).sort({"mentors.mentee":1}).pretty()
+
+//6. Find the number of users who are absent and task is not submitted  between 15 oct-2020 and 31-oct-2020
+db.zenclassdata.find({"attendance.date":{$gte:15, $lte:31}},{_id:0,attendance:{absent:1,date:1},tasks:{notsubmit:1}}).pretty()
